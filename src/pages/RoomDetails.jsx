1a9fc2c96cdf9c410fff22ebc8a811d58452ec75
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-11-09 11:28:46 +0600

    implemented review post functionality

--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -6,6 +6,7 @@ import { Helmet } from "react-helmet";
 import { FaCalendarAlt } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+import Review from "../components/reviews/Review";
 import useAuth from "../hooks/useAuth";
 import useAxios from "../hooks/useAxios";
 
@@ -66,62 +67,65 @@ const RoomDetails = () => {
   };
 
   return (
-    <div className="bg-white rounded shadow-md flex flex-col md:flex-row items-center gap-10 my-10">
+    <div className="py-10">
       <Helmet>
         <title>Room Details | The Luxe Haven</title>
       </Helmet>
-      <div className="flex-1">
-        <img
-          src={item?.data?.img}
-          alt={item?.data?.name}
-          className="rounded-l"
-        />
-      </div>
-      <div className="flex-1 ">
-        <div className="p-5 space-y-1">
-          <h1 className="text-2xl font-bold">{item?.data?.name}</h1>
-          <p>
-            <span className="font-bold">Price Per Night: </span>$
-            {item?.data?.price_per_night}
-          </p>
-          <p>
-            <span className="font-bold">Room Size: </span>
-            {item?.data?.room_size}
-          </p>
-          <p>
-            <span className="font-bold">Description: </span>
-            {item?.data?.desc}
-          </p>
-          <p>
-            <span className="font-bold">Special Offers: </span>
-            {item?.data?.special_offers}
-          </p>
+      <div className="bg-white rounded shadow-md flex flex-col md:flex-row items-center gap-10 mb-10">
+        <div className="flex-1">
+          <img
+            src={item?.data?.img}
+            alt={item?.data?.name}
+            className="rounded-l"
+          />
         </div>
-        <div className="flex justify-center gap-5">
-          <label className="flex items-center border border-primary hover:border-secondary rounded cursor-pointer px-3">
-            <DatePicker
-              selected={selectDate}
-              onChange={(date) => setSelectDate(date)}
-              className="outline-0 border-none"
-            />
-            <FaCalendarAlt />
-          </label>
-          {!user ? (
-            <Link to="/login">
-              <button className="bg-primary hover:bg-secondary transition-all duration-500 p-2 rounded uppercase text-white font-medium">
+        <div className="flex-1 ">
+          <div className="p-5 space-y-1">
+            <h1 className="text-2xl font-bold">{item?.data?.name}</h1>
+            <p>
+              <span className="font-bold">Price Per Night: </span>$
+              {item?.data?.price_per_night}
+            </p>
+            <p>
+              <span className="font-bold">Room Size: </span>
+              {item?.data?.room_size}
+            </p>
+            <p>
+              <span className="font-bold">Description: </span>
+              {item?.data?.desc}
+            </p>
+            <p>
+              <span className="font-bold">Special Offers: </span>
+              {item?.data?.special_offers}
+            </p>
+          </div>
+          <div className="flex justify-center gap-5">
+            <label className="flex items-center border border-primary hover:border-secondary rounded cursor-pointer px-3">
+              <DatePicker
+                selected={selectDate}
+                onChange={(date) => setSelectDate(date)}
+                className="outline-0 border-none"
+              />
+              <FaCalendarAlt />
+            </label>
+            {!user ? (
+              <Link to="/login">
+                <button className="bg-primary hover:bg-secondary transition-all duration-500 p-2 rounded uppercase text-white font-medium">
+                  Book Now
+                </button>
+              </Link>
+            ) : (
+              <button
+                onClick={handleBooking}
+                className="bg-primary hover:bg-secondary transition-all duration-500 p-2 rounded uppercase text-white font-medium"
+              >
                 Book Now
               </button>
-            </Link>
-          ) : (
-            <button
-              onClick={handleBooking}
-              className="bg-primary hover:bg-secondary transition-all duration-500 p-2 rounded uppercase text-white font-medium"
-            >
-              Book Now
-            </button>
-          )}
+            )}
+          </div>
         </div>
       </div>
+      <Review />
     </div>
   );
 };
